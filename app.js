d203a60491630684a3c07ec61923e5ab026877d2
Paulescu Maria-Alexandra <paulescumariaalexandra@gmail.com>
2021-12-20 22:23:19 +0200

    feat: Create endpoint for create a recipe

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const route = require('../express-app/Architecture/Routes/appRoutes')
 
 mongoose.connect('mongodb://localhost/cookbookdb', { useNewUrlParser: true})
 const db = mongoose.connection
@@ -8,5 +9,7 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
+
+app.use('/api', route)
 
 app.listen(8080, () => console.log('Server Started'))
